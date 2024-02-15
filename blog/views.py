17c7bf2cf8571f19edc331c0c8de8045b1c7bc37
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-02-15 00:08:09 -0600

    posts by similarity, taggin system

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
 
 from blog.models import Post, Comment
 from blog.forms import EmailPostForm, CommentForm
@@ -11,8 +13,13 @@ from blog.forms import EmailPostForm, CommentForm
 
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -25,7 +32,8 @@ def post_list(request):
         request,
         'blog/post/list.html',
         {
-            'posts': posts
+            'posts': posts,
+            'tag': tag
         }
     )
 
@@ -41,13 +49,19 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
+        '-same_tags', '-publish'
+    )[:4]
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
