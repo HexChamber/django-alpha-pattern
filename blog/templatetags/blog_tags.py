efb1d40c2a03f23261ec0b88ded60c75b9896117
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-02-15 02:04:43 -0600

    latest posts inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
+
 
 
 register = template.Library()
@@ -14,3 +16,14 @@ def indent_text(text, classes=''):
         text += '</p>'
 
     return format_html(text)
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
